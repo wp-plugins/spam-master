ba47e13c3980b3e2bbdb0b555abde6301503c5b6
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-28 21:56:27 +0000

    version 4.2.4
    git-svn-id: https://plugins.svn.wordpress.org/spam-master/tags/4.2.4@847268 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ spam-master.php
@@ -0,0 +1,60 @@
+<?php
+/**
+Plugin Name: Spam Master
+Plugin URI: http://wordpress.techgasp.com/spam-master/
+Version: 4.2.4
+Author: TechGasp
+Author URI: http://wordpress.techgasp.com
+Text Domain: spam-master
+Description: Spam Master is the Ultimate Spam Protection plugin that blocks new user registrations and post comments with Real Time anti-spam lists.
+License: GPL2 or later
+*/
+/* Copyright 2013 TechGasp (email : info@techgasp.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+///////DEFINE VERSION///////
+define( 'spam_master_VERSION', '4.2.4' );
+$spam_master_version = "4.2.4"; //for other pages
+if( is_multisite() ) {
+update_site_option( 'spam_master_version', $spam_master_version );
+}
+else{
+update_option( 'spam_master_version', $spam_master_version );
+}
+// HOOK ADMIN
+require_once( dirname( __FILE__ ) . '/includes/spam-master-admin.php');
+// HOOK SETTINGS
+require_once( dirname( __FILE__ ) . '/includes/spam-master-settings.php');
+// HOOK Re-CAPTACHA & HONEYPOT
+//require_once( dirname( __FILE__ ) . '/includes/spam-master-recaptcha.php');
+// HOOK THREATS
+require_once( dirname( __FILE__ ) . '/includes/spam-master-threats.php');
+// HOOK INVITATION
+require_once( dirname( __FILE__ ) . '/includes/spam-master-invite.php');
+// HOOK REGISTRATIONS
+require_once( dirname( __FILE__ ) . '/includes/spam-master-registrations.php');
+// HOOK BLOCKS
+require_once( dirname( __FILE__ ) . '/includes/spam-master-blocks.php');
+
+add_filter( 'plugin_action_links', 'spam_master_links', 10, 2 );
+//SETTINGS LINK IN PLUGIN MANAGER
+function spam_master_links( $links, $file ) {
+	if ( $file == plugin_basename( dirname(__FILE__).'/spam-master.php' ) ) {
+		$links[] = '<a href="' . admin_url( 'admin.php?page=spam-master' ) . '">'.__( 'Settings' ).'</a>';
+	}
+
+	return $links;
+}
+?>
